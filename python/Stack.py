90eb9c20c81b9c6671313199cda67f48f1336d27
Hamza Waseem <hamzawaseem_7@live.com>
2020-05-17 19:53:53 -0400

    Python/Stack: Completed
    Stack defintion completed
    Initial testing performed

--- python/Stack.py
+++ python/Stack.py
@@ -34,22 +34,45 @@ class Stack:
         self._size = 0
 
     def __str__(self):
+        """
+        Returns a string representation of the stack
+
+        """
         if (self._size == 0):
             return "None"
         
         _str = ""
+        top = True
         for elem in reversed(self._int_list):
-            _str += elem + " "
+            _str += str(elem) + " "
+            if top:
+                _str += "| "
+                top = False
         return _str
 
     def __repr__(self):
+        """
+        See __str__()
+
+        """
         return self.__str__()
 
     def push(self, value):
+        """
+        Add an item to the top of the stack
+        Increments size by 1
+
+        """
         self._int_list.append(value)
         self._size += 1
 
     def pop(self):
+        """
+        Removes and returns the item at the top of the stack
+        Decreases size by 1
+        Returns None if empty
+
+        """
         if (self._size == 0):
             return None
 
@@ -57,10 +80,18 @@ class Stack:
         return self._int_list.pop()
 
     def top(self):
+        """
+        Returns the item at the top of the stack
+        Returns None if empty
+        """
         if (self._size == 0):
             return None
 
         return self._int_list[-1]
 
     def size(self):
+        """
+        Returns the size of the stack
+
+        """
         return self._size
